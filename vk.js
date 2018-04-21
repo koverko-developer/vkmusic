605ddf0216b13bde4e7ae4f789f308c0b8d04908
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-21 19:49:23 +0300

    Update vk.js

--- vk.js
+++ vk.js
@@ -8,14 +8,23 @@ var iconv = require('iconv-lite');
 var USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36';
 
 
-https.post('https://vk.com/al_audio.php', (res) => {
+const options = {
+  hostname: 'encrypted.google.com',
+  port: 443,
+  path: '/',
+  method: 'GET'
+};
+
+const req = https.request(options, (res) => {
   console.log('statusCode:', res.statusCode);
   console.log('headers:', res.headers);
 
   res.on('data', (d) => {
     process.stdout.write(d);
   });
+});
 
-}).on('error', (e) => {
+req.on('error', (e) => {
   console.error(e);
 });
+req.end();
