f3292c39aab82129518d1c33c6473dd3b5503cff
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-22 02:56:12 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
  var afterLoad=require('after-load');
 
-var t = afterLoad('https://google.com',function(html){
+afterLoad('https://google.com',function(html){
      console.log(html);
  });
-
-console.log(t);
