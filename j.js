4360a16fac2e6772d857481bfea6a3fe3b556a76
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-21 23:22:14 +0300

    Update j.js

--- j.js
+++ j.js
@@ -5,6 +5,13 @@ var cheerio = require('cheerio');
 
 nightmare
   .goto('https://vk.com')
+    .evaluate(function(){
+
+        //here is where I want to return the html body
+        return document.html;
+
+
+    })
   .then(function(body){
     //loading html body to cheerio
         var b = cheerio.load(body);
