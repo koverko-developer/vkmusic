f87e3a13958b36238166a658b933022eda8dd68b
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-21 19:46:23 +0300

    Update vk.js

--- vk.js
+++ vk.js
@@ -2,9 +2,9 @@ var querystring = require('querystring');
 var https = require('https');
 var iconv = require('iconv-lite');
 
-var my_id = parseInt(readline.question("Enter your ID: "));
+//var my_id = parseInt(readline.question("Enter your ID: "));
 
-var COOKIE = readline.question("Enter your cookies: ");
+//var COOKIE = readline.question("Enter your cookies: ");
 var USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36';
 
 
