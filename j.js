6495cbd349840eb9f7e4f28bb2672941c0e6a1f7
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-21 23:18:40 +0300

    Update j.js

--- j.js
+++ j.js
@@ -1,11 +1,16 @@
 const Nightmare = require('nightmare')
 const nightmare = Nightmare({ show: true })
+var cheerio = require('cheerio');
 
 
 nightmare
   .goto('https://vk.com')
   .end()
-  .then(console.log)
+  .then(function(body){
+  //loading html body to cheerio
+      var $ = cheerio.load(body);
+      console.log(body);
+  })
   .catch(error => {
     console.error('Search failed:', error)
   })
