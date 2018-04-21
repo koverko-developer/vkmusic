518e5cce620ae828b0cf0442e08963676b14d45e
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-21 23:32:46 +0300

    Update j.js

--- j.js
+++ j.js
@@ -1,16 +1,14 @@
-var Nightmare1 = require('nightmare');
-var nightmare = Nightmare1({
-  show: true
-});
-var fs = require('fs');
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({ show: true })
 
 nightmare
-  .goto('http://vk.com')
-  .evaluate(function() {
-    return document.querySelector('body')
-      .innerHTML;
-  })
+  .goto('https://duckduckgo.com')
+  .type('#search_form_input_homepage', 'github nightmare')
+  .click('#search_button_homepage')
+  .wait('#r1-0 a.result__a')
+  .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
   .end()
-  .then(function(page) {
-      console.log(page);
-    });
+  .then(console.log)
+  .catch(error => {
+    console.error('Search failed:', error)
+  })
