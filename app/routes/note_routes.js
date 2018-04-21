6dd6f124eb0ae64f7fb94299ad030e95aa103009
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-21 18:18:28 +0300

    Update note_routes.js

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,49 +1,9 @@
 var cheerio = require('cheerio');
 var iconv = require('iconv-lite');
 
-module.exports = function(app, cookie,iconv, request, querystring) {
+module.exports = function(app, cookie,iconv, request, querystring) ; 
 
-  app.get('/audio/:id', (req, res) => {
-    const id = req.params.id;
-    request.post({
-        headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
-        'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
-         url:     'https://vk.com/al_audio.php',
-        form: "access_hash=&act=load_section&al=1&offset=0&owner_id="+id+"&playlist_id=-1&type=playlist",
-      }, function(error, response, body){
-        if(!error){
-          //res.send(body)
-          parse(body, res);
-        }else {
-          console.log('MY ERR:----------'+error);
-          res.send(error)
-        }
-
-      });
-    });
-
-  app.get('/audio/:id/:offset/', (req, res) => {
-
-    const id = req.params.id;
-    const offset = req.params.offset;
-    request.post({
-        headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
-        'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
-         url:     'https://vk.com/al_audio.php',
-        form: "access_hash=&act=load_section&al=1&limit=30&offset="+offset+"&owner_id="+id+"&playlist_id=-1&type=playlist",
-      }, function(error, response, body){
-        if(!error){
-          //res.send(body)
-          parse(body, res);
-        }else {
-          console.log('MY ERR:----------'+error);
-          res.send('err')
-        }
-
-      });
-  });
-
-  app.get('/audioSpecial/:id', (req, res) => {
+app.get('/audioSpecial/:id', (req, res) => {
     const id = req.params.id;
     request.post({
         headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
@@ -62,84 +22,6 @@ module.exports = function(app, cookie,iconv, request, querystring) {
       });
   });
 
-  app.get('/audioNews/:id', (req, res) => {
-    const id = req.params.id;
-    request.post({
-        headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
-        'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
-        url:     'https://vk.com/al_audio.php?',
-        form: "access_hash=&act=load_section&al=1&claim=0&owner_id="+id+"&playlist_id=recomsPUkLGlpXADkvD0tMBABHRDYKDhNqQBIWI0lTVFZVHwcqBA5USA&type=recoms",
-      }, function(error, response, body){
-        if(!error){
-          //res.send(body)
-          console.log('BODY:----------'+body)
-          parse(body, res);
-        }else {
-          console.log('MY ERR:----------');
-          res.send(error)
-        }
-      });
-  });
-
-  app.get('/audioPopular/:id', (req, res) => {
-    const id = req.params.id;
-    request.post({
-        headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
-        'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
-        url:     'https://vk.com/al_audio.php?',
-        form: "act=recoms_blocks&al=1",
-      }, function(error, response, body){
-        if(!error){
-          //res.send(body)
-          console.log('BODY:----------'+body)
-          parse(body, res);
-        }else {
-          console.log('MY ERR:----------');
-          res.send(error)
-        }
-      });
-  });
-
-  app.get('/audioGroup/:id/', (req, res) => {
-    const id = req.params.id;
-    request.post({
-        headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
-        'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
-        url:     'https://vk.com/al_wall.php?',
-        form: "access_hash=&act=get_wall&al=1&owner_id="+id+"&type=own&offset=0&wall_start_from=0",
-      }, function(error, response, body){
-        if(!error){
-          //res.send(body)
-          console.log('BODY:----------'+body)
-          parse(body, res);
-        }else {
-          console.log('MY ERR:----------');
-          res.send(error)
-        }
-      });
-  });
-  
-  app.get('/insta', (req, res) => {
-    request.post({
-        headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
-        'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
-        url:     'https://www.instagram.com/',
-        form: "access_hash=&act=get_wall&al=1&owner_id=7"+"&type=own&offset=0&wall_start_from=0",
-      }, function(error, response, body){
-        if(!error){
-          res.send(body)
-          console.log('BODY:----------'+body.substring(0,1000));
-          //parse(body, res);
-        }else {
-          console.log('MY ERR:----------');
-          res.send(error)
-        }
-      });
-  });
-
-
-
-}
 
 function parse(body, res){
   console.log('parse');
@@ -155,7 +37,7 @@ function parse(body, res){
   //res.send('hello')
   }catch(err){
   console.log('MY ERR rapse:----------'+ err);
-  res.send(err)
+  res.send(err);
   }
 
 }
