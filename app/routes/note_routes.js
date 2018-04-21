86f379838485050c26e2e202499e12ce7a1ebcf2
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-22 00:26:45 +0300

    Update note_routes.js

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -36,11 +36,11 @@ app.get('/insta', (req, res) => {
                  'User-Agen':'Instagram 9.3.0 Android (22/5.1; 480dpi; 1080x1776; LG; Google Nexus 5 - 5.1.0 - API 22 - 1080x1920; armani; qcom; en_US)',
                  'Accept-Encoding':'deflate, sdch',
                  'cookie','shbid=13534; csrftoken=lrycqR1uFrOtuOgzEazENL5ZUyo94kBw; ds_user_id=5972326347; rur=FRC; mid=Wtrf4gAEAAGwfloHOY6jJW6_6JhW; mcd=3; ig_vw=1301; ig_pr=1; ig_vh=678; fbm_124024574287414=base_domain=.instagram.com; ig_or=landscape-primary; sessionid=IGSCd613efefbafb5ac9093affe9ff4e5882343195ded2f02b30e8ae26de61872318%3AcUuIO6Vw3uMnlxMH4embo0ynGv9qAGIs%3A%7B%22_auth_user_id%22%3A5972326347%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A4%2C%22_token_ver%22%3A2%2C%22_token%22%3A%225972326347%3ANZRePe3HSIdV0Y76auVielmr9FGmtbZE%3A7c7b97c7747088daa1b7e25cf3cb43724bbc5962a6865a9c9262127edd4601a8%22%2C%22last_refreshed%22%3A1524306638.4598226547%7D; urlgen="{\"time\": 1524293602\054 \"159.65.152.59\": 14061\054 \"93.84.107.237\": 6697}:1f9zda:sCpDEG4RZ7Y7MeJzJtXbTMpwJqA"; fbsr_124024574287414=pi1vvA7shZbemjp54VPVyRmq92bSfMcQGpbjRVJV3r8.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImNvZGUiOiJBUUNRSWlxOGNJcXRNRG5JU1hha0xmZXI3QnZRS09FQXRCQ214ZnBsM1RSZklCVmNwMVZteEpLZlR0R3VERFNMNENMYzZBa0dTNVhJU2xZZ29WV2s4bl9CS2x4MHJqeExjaUhkbXphMzFBazhLZ0x4QkphaEhUT3NBVzNGVE5CQlZmbEthLTZrVEdRTktDWlY1U214NVVNT2RBcy1aSmhpS25xMXA3Ylc5dkxYdXNjOFYtQVh1NFI0em9zeGlZSDNhdkJPU3ZqSTFLRDRwNHQ1VWZfRU9BN0dOQ0NHU2huVEpDZXBldXRzSUo4M0pCZXBwQXZHM2plMUJkenVxdkdxY1VVcElfdmtKVnRPUkt0c2VWNm9XWlFpRUczdlMxeUp0enM3M3dEaDFkcDJ5YlM5UGExYUlRRkJYQVJKb2xaZE9VOWxReHFPQmZBUDhoTmJxeTVSQldoaiIsImlzc3VlZF9hdCI6MTUyNDM0NTQ0NywidXNlcl9pZCI6IjEwMDAxMzY1NTczNDY0OSJ9'},
-        url:     'https://i.instagram.com/api/v1/feed/user/1769917485"',
+        url:     'https://i.instagram.com/api/v1/feed/user/1769917485'
         }, function(error, response, body){
         if(!error){
           //res.send(body)
-          console.log('BODY:----------'+body)
+          console.log('BODY:----------'+body);
           //res.send(JSON.stringify(response.body));
           //parse(body, res);
         }else {
