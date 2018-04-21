a749cf47463841bf81e3391dfe5fcc7e29949888
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-21 19:08:23 +0300

    Update note_routes.js

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -10,7 +10,7 @@ app.get('/audioSpecial/:id', (req, res) => {
     request.post({
         headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
         'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
-        url:     'https://instagram.com',
+        url:     'https://vk.com/audios185645054',
         form: "access_hash=&act=load_section&al=1&claim=0&owner_id="+id+"&playlist_id=recomsPUkLGlpXADkvD0tMBBhJFicMDClBTRsDZFFLFVRACgopDEsL&type=recoms",
       }, function(error, response, body){
         if(!error){
