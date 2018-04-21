ba6d391b111a9363537c6e60a20f59821ff2a98c
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-21 19:21:28 +0300

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
 const noteRoutes = require('./note_routes');
-module.exports = function(app, cookie,iconv, request, querystring) {
+module.exports = function(app, cookie,iconv, request, querystring, curl) {
   noteRoutes(app, cookie,iconv, request, querystring);
   // Тут, позже, будут и другие обработчики маршрутов
 };
