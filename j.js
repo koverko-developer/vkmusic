da544f01cea3571ef32a979a218af9fd5fc7fb14
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-21 23:40:21 +0300

    Update j.js

--- j.js
+++ j.js
@@ -2,19 +2,13 @@ const Nightmare = require('nightmare')
 const nightmare = Nightmare({ show: true })
 
 nightmare
-    // it *really* goes to this URL, which will fail
-    .goto('www.vk.com')
-    .catch(function(error) {
-        // only handle errors from the `evaluate` action
-        if (error.message && error.message.includes('Evaluation')) {
-            return nightmare
-                // now we'll never get here if the page failed to load, so no
-                // need to worry about this `evaluate` call timing out.
-                .evaluate(() => document.documentElement.outerHTML)
-                .then(function(html) { 
-                 console.log(html);
-                                     });
-        }
-        throw error;
-    })
-    .catch(function(error) { console.error(error); });
+    .goto('http://www.vk.com/')
+  .wait()
+  .evaluate(function () {
+        return document.documentElement.outerHTML;
+     }, function (result) {
+        console.log( result);
+     }
+  ).run(function( err, nightmare){
+    console.log("done");
+  });
