571a92948a42a953d536c455d749caa3d8d795af
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-22 18:16:50 +0300

    Update note_routes.js

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -130,7 +130,7 @@ module.exports = function(app, cookie,iconv, request, querystring) {
     var options = {
 	 headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
         'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
-         uri:     'https://vk.com'};
+         uri:     'https://vk.com/feed'};
 	  
     rp(options)
     .then((data) => {
