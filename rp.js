0a7cdac7a97d6e0d5c068d716b868f5ee8094eb2
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-22 16:57:39 +0300

    Update rp.js

--- rp.js
+++ rp.js
@@ -1,18 +1,55 @@
-var cheerio = require('cheerio'); // Basically jQuery for node.js
-var rp = require('request-promise');
- 
-var options = {
-    uri: 'http://www.google.com',
-    transform: function (body) {
-        consloe.log(cheerio.load(body));
-        return cheerio.load(body);
-    }
-};
- 
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const Table = require('cli-table');
+
+let table = new Table({
+	head: ['username', '❤️', 'challenges'],
+	colWidths: [15, 5, 10]
+})
+
+const options = {
+	url: `https://forum.freecodecamp.org/directory_items?period=weekly&order=likes_received&_=1518604435748`,
+	json: true
+}
+
 rp(options)
-    .then(function ($) {
-        // Process html like you would with jQuery...
-    })
-    .catch(function (err) {
-        // Crawling failed or Cheerio choked...
-    });
+	.then((data) => {
+		let userData = [];
+		for (let user of data.directory_items) {
+			userData.push({name: user.user.username,likes_received: user.likes_received});
+		}
+		process.stdout.write('loading');
+		getChallengesCompletedAndPushToUserArray(userData);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+function getChallengesCompletedAndPushToUserArray(userData) {
+	var i = 0;
+	function next() {
+		if (i < userData.length) {
+			var options = {
+				url: `https://www.freecodecamp.org/` + userData[i].name,
+				transform: body => cheerio.load(body)
+			}
+			rp(options)
+				.then(function ($) {
+					process.stdout.write(`.`);
+					const fccAccount = $('h1.landing-heading').length == 0;
+					const challengesPassed = fccAccount ? $('tbody tr').length : 'unknown';
+					table.push([userData[i].name, userData[i].likes_received, challengesPassed]);
+					++i;
+					return next();
+				})
+		} else {
+			printData();
+		}
+	}
+	return next();
+};
+
+function printData() {
+	console.log("✅");
+	console.log(table.toString());
+}
