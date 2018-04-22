5822b5e384e1365ed03fb78eb1d15be034ab4ea5
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-22 17:21:17 +0300

    Update note_routes.js

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -125,7 +125,13 @@ module.exports = function(app, cookie,iconv, request, querystring) {
       });
   });
 	
-  app.get('/vk', (req, res) =>{    
+  app.get('/vk', (req, res) =>{  
+    var options = {
+	headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
+        'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
+         url:     'https://vk.com/al_audio.php',
+         form: "access_hash=&act=load_section&al=1&offset=0&owner_id="+id+"&playlist_id=-1&type=playlist"};
+	  
     rp(options)
     .then((data) => {
       console.log(data);
