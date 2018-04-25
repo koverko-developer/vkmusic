d5ed56c1beb9e2fe24c090c9e45f36eafdc28e9a
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-25 17:02:38 +0300

    Update note_routes.js

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,22 +1,119 @@
 var cheerio = require('cheerio');
 var iconv = require('iconv-lite');
-var cookie = "remixlang=0; remixstid=174780143_8f08d9b6cecc032d5b; remixflash=0.0.0; remixscreen_depth=24; remixdt=0; remixttpid=da52df1d140a2a76ab14e8be1b9adea7d54a1e6835; remixmdevice=1366/768/1/!!-!!!!; remixseenads=0; remixrefkey=d1fb7610a21ed037de; remixgp=55e4345b0f6326e4e5d01b97afeffe5c; remixsid=ae802088f9843990d5fe6e06cabe92f94dfedfd9884f7ec723be4; remixcurr_audio=2000386672_456239911; tmr_detect=0%7C1524664778348";
-var request = require('request');
-//const Nightmare = require('nightmare')
-//const puppeteer = require('puppeteer');
-//var cookie = require('cookie-parse');
+const Nightmare = require('nightmare')
+const puppeteer = require('puppeteer');
+var cookie = require('cookie-parse');
 module.exports = function(app, cookie,iconv, request, querystring) {
 
- 
+  app.get('/audio/:id', (req, res) => {
+    const id = req.params.id;
+    var a = sendP(request,id);
+    });
+
+  app.get('/audio/:id/:offset/', (req, res) => {
+
+    const id = req.params.id;
+    const offset = req.params.offset;
+    request.post({
+        headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
+        'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
+         url:     'https://vk.com/al_audio.php',
+        form: "access_hash=&act=load_section&al=1&limit=30&offset="+offset+"&owner_id="+id+"&playlist_id=-1&type=playlist",
+      }, function(error, response, body){
+        if(!error){
+          //res.send(body)
+          parse(body, res);
+        }else {
+          console.log('MY ERR:----------'+error);
+          res.send('err')
+        }
+
+      });
+  });
+
   app.get('/audioSpecial/:id', (req, res) => {
     const id = req.params.id;
-    sendP(id, cookie, request, res);     
-  });  
+    sendP(id, cookie, request, res);
+    // res.send('');
+    // request.post({
+    //     headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
+    //     'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
+    //     url:     'https://vk.com/al_audio.php?',
+    //     form: "access_hash=&act=load_section&al=1&claim=0&owner_id="+id+"&playlist_id=recomsPUkLGlpXADkvD0tMBBhJFicMDClBTRsDZFFLFVRACgopDEsL&type=recoms",
+    //   }, function(error, response, body){
+    //     if(!error){
+    //       //res.send(body)
+    //       console.log('BODY:----------'+body)
+    //       parse(body, res);
+    //     }else {
+    //       console.log('MY ERR:----------');
+    //       res.send(error)
+    //     }
+    //   });
+  });
+
+  app.get('/audioNews/:id', (req, res) => {
+    const id = req.params.id;
+    request.post({
+        headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
+        'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
+        url:     'https://vk.com/al_audio.php?',
+        form: "access_hash=&act=load_section&al=1&claim=0&owner_id="+id+"&playlist_id=recomsPUkLGlpXADkvD0tMBABHRDYKDhNqQBIWI0lTVFZVHwcqBA5USA&type=recoms",
+      }, function(error, response, body){
+        if(!error){
+          console.log('BODY:----------'+body)
+          parse(body, res);
+        }else {
+          console.log('MY ERR:----------');
+          res.send(error)
+        }
+      });
+  });
+
+  app.get('/audioPopular/:id', (req, res) => {
+    const id = req.params.id;
+    request.post({
+        headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
+        'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
+        url:     'https://vk.com/al_audio.php?',
+        form: "act=recoms_blocks&al=1",
+      }, function(error, response, body){
+        if(!error){
+          //res.send(body)
+          console.log('BODY:----------'+body)
+          parse(body, res);
+        }else {
+          console.log('MY ERR:----------');
+          res.send(error)
+        }
+      });
+  });
+
+  app.get('/audioGroup/:id/', (req, res) => {
+    const id = req.params.id;
+    request.post({
+        headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
+        'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
+        url:     'https://vk.com/al_wall.php?',
+        form: "access_hash=&act=get_wall&al=1&owner_id="+id+"&type=own&offset=0&wall_start_from=0",
+      }, function(error, response, body){
+        if(!error){
+          //res.send(body)
+          console.log('BODY:----------'+body)
+          parse(body, res);
+        }else {
+          console.log('MY ERR:----------');
+          res.send(error)
+        }
+      });
+  });
+
+  app.get('/vk', (req, res) =>{
+      var a = sendP(id, cookie);
+      res.send(a);
+    });
 }
 async function sendP(id,cookie, request, res){
- 
-  console.log(cookie);
- 
   request.post({
       headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
       'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
@@ -24,8 +121,7 @@ async function sendP(id,cookie, request, res){
       form: "access_hash=&act=load_section&al=1&offset=0&owner_id="+id+"&playlist_id=-1&type=playlist",
     }, function(error, response, body){
       if(!error){
-        console.log(body);
-        
+        //console.log(body);
         var a = parse(body, response);
         res.send(a);
       }else {
@@ -35,24 +131,23 @@ async function sendP(id,cookie, request, res){
       }
 
     });
-
 }
+
 function parse(body, res){
   console.log('parse');
   try{
-  var res1 = body;
-  var tmpJson = res1.substring(res1.indexOf("gridCovers") + 7, res1.indexOf("hasMore") +8);
+  let res1 = body;
+  let tmpJson = res1.substring(res1.indexOf("gridCovers") + 7, res1.indexOf("hasMore") +8);
   console.log(tmpJson.indexOf("list"));
   console.log(tmpJson.indexOf("hasMore"));
-  var arrL = tmpJson.substring(tmpJson.indexOf("list")+6, tmpJson.length-6);
-  var listAudio = arrL.substring(0, arrL.length -4);
-  var jsonStringArray = iconv.decode(listAudio,'win1251');
-  var jsonSTR = JSON.stringify(jsonStringArray);
-  var json = JSON.parse(jsonStringArray);
+  let arrL = tmpJson.substring(tmpJson.indexOf("list")+6, tmpJson.length-6);
+  let listAudio = arrL.substring(0, arrL.length -4);
+  let jsonStringArray = iconv.decode(listAudio,'win1251');
+  let jsonSTR = JSON.stringify(jsonStringArray);
+  let json = JSON.parse(jsonStringArray);
   return json;
   //res.send(json)
   }catch(err){
-   return 'err';
   console.log('MY ERR rapse:----------');
   //res.send(err)
   }
