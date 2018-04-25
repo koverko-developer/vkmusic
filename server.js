73adbab15572dab3f137fa45350b324d6d4bfc44
koverko-developer <kowerkodeveloper@gmail.com>
2018-04-25 17:00:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var request = require('request');
 var iter = 0;
 var my_id = parseInt("185645054");
 
-var cookie = "remixlang=0; remixstid=174780143_8f08d9b6cecc032d5b; remixflash=0.0.0; remixscreen_depth=24; remixdt=0; remixttpid=da52df1d140a2a76ab14e8be1b9adea7d54a1e6835; remixmdevice=1366/768/1/!!-!!!!; remixseenads=0; remixrefkey=d1fb7610a21ed037de; remixgp=55e4345b0f6326e4e5d01b97afeffe5c; remixsid=ae802088f9843990d5fe6e06cabe92f94dfedfd9884f7ec723be4; tmr_detect=0%7C1524664286097; remixcurr_audio=2000386058_456240006";
+var cookie = "remixlang=0; remixstid=174780143_8f08d9b6cecc032d5b; remixflash=0.0.0; remixscreen_depth=24; remixdt=0; remixttpid=da52df1d140a2a76ab14e8be1b9adea7d54a1e6835; remixmdevice=1366/768/1/!!-!!!!; remixseenads=0; remixrefkey=d1fb7610a21ed037de; remixgp=55e4345b0f6326e4e5d01b97afeffe5c; remixsid=ae802088f9843990d5fe6e06cabe92f94dfedfd9884f7ec723be4; remixcurr_audio=2000386672_456239911; tmr_detect=0%7C1524664778348";
 
 var options = {
   url: 'https://vk.com/al_audio.php?__query=audios185645054&_ref=left_nav&_tstat=274%2C0%2C43%2C322%2Cgroups_list&al=-1&al_id=185645054&_rndVer=63236',
