74f22ebfec412175d5b79186a018183c8d25804c
koverko-developer <kowerkodeveloper@gmail.com>
2018-07-27 17:54:17 +0300

    Add files via upload

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,10 +1,15 @@
 var cheerio = require('cheerio');
 var iconv = require('iconv-lite');
+var needle = require('needle');
+
 module.exports = function(app, cookie,iconv, request, querystring) {
 
   app.get('/audio/:id', (req, res) => {
     const id = req.params.id;
-    var a = sendP(request,id);
+    //res.send('dasdad');
+
+    getAudio(res, req, id, cookie);
+    //sendP(request,id);
     });
 
   app.get('/audio/:id/:offset/', (req, res) => {
@@ -98,7 +103,8 @@ module.exports = function(app, cookie,iconv, request, querystring) {
     });
 }
 async function sendP(id,cookie, request, res){
-  request.post({
+
+  /*axios.post({
       headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
       'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '},
        url:     'https://vk.com/al_audio.php',
@@ -114,26 +120,73 @@ async function sendP(id,cookie, request, res){
         res.send('error')
       }
 
-    });
+    });*/
+    res.send('helo');
+}
+function getAudio(res, req, id, cookie){
+  try{
+    var options = {
+        headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36',
+          'cookie' : cookie,'content-type' : 'application/x-www-form-urlencoded','content-type': 'application/x-www-form-urlencoded;charset=windows-1251 '}
+      }
+    var data = {
+      access_hash : '',
+      act : 'load_section',
+      al : '1',
+      offset : '0',
+      owner_id : id,
+      playlist_id : '-1',
+      type: 'playlist'
+    }
+    var a = needle.post('https://vk.com/al_audio.php', data, options);
+    var  resp = '';
+    a.on('readable', function() {
+
+      while (data = this.read()) {
+        console.log(data.toString());
+        resp = resp + data.toString();
+      }
+    })
+    a.on('done', function(err) {
+      // if our request had an error, our 'done' event will tell us.
+      if (!err) console.log('Great success!');
+      var a = parse(resp, res);
+      //res.send('done');
+    })
+  }
+  catch(err) {
+    console.log('Call the locksmith!')
+    res.send('errr')
+  }
 }
 
 function parse(body, res){
+  console.log(body);
   console.log('parse');
   try{
   let res1 = body;
+  var fs = require('fs');
   let tmpJson = res1.substring(res1.indexOf("gridCovers") + 7, res1.indexOf("hasMore") +8);
-  console.log(tmpJson.indexOf("list"));
-  console.log(tmpJson.indexOf("hasMore"));
+  //console.log(tmpJson);
+  console.log('list= ' + tmpJson.indexOf("list"));
+  console.log('hasMore= ' + tmpJson.indexOf("hasMore"));
   let arrL = tmpJson.substring(tmpJson.indexOf("list")+6, tmpJson.length-6);
+
   let listAudio = arrL.substring(0, arrL.length -4);
-  let jsonStringArray = iconv.decode(listAudio,'win1251');
-  let jsonSTR = JSON.stringify(jsonStringArray);
-  let json = JSON.parse(jsonStringArray);
-  return json;
-  //res.send(json)
+  fs.writeFile("/home/mobi-app/development/node/vk/test.txt", listAudio, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+
+      console.log("The file was saved!");
+  });
+  //let jsonStringArray = iconv.decode(listAudio,'win1251');
+  //let jsonSTR = JSON.stringify(jsonStringArray);
+  let json = JSON.parse(listAudio);
+  res.send(json)
   }catch(err){
   console.log('MY ERR rapse:----------');
-  //res.send(err)
+  res.send(err)
   }
 
 }
