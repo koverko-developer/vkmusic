e752c2d5f084bf1a9de1eb735d1fcf2b36c14f12
koverko-developer <kowerkodeveloper@gmail.com>
2018-07-27 18:30:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var options = {
   }
 };
 
-const port = 3000;
+var port = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 require('./app/routes')(app, cookie,iconv, request, querystring);
 app.listen(port, () => {
